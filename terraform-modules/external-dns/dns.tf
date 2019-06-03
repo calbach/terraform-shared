a0bb45515cc10dfbf3d44d3a5f3d94e2756210d6
Mark Sarcevicz <smark@broadinstitute.org>
2019-06-03 11:52:40 -0400

    fixing

--- terraform-modules/external-dns/dns.tf
+++ terraform-modules/external-dns/dns.tf
@@ -1,11 +1,11 @@
-data "google_dns_managed_zone" "${var.target_dns_resource_name}" {
+data "google_dns_managed_zone" "dns_zone" {
     provider     = "google.targetdns"
     project      = "${var.target_project}"
     name         = "${var.target_dns_zone_name}"
 }
 resource "google_dns_record_set" "set-record" {
   provider     = "google.targetdns"
-  managed_zone = formatlist("${data.google_dns_managed_zone.%s.name}", var.target_dns_resource_name)
+  managed_zone = "${data.google_dns_managed_zone.dns_zone.name}"
 #  name         = "${var.a_record_name}.${data.google_dns_managed_zone.${var.target_dns_resource_name}.dns_name}"
 #  type         = "A"
   ttl          = "300"
