e6074052363e45b5968fae966de3b97ba5311630
Mark Sarcevicz <smark@broadinstitute.org>
2019-06-03 10:28:38 -0400

    removing weird work around

--- terraform-modules/external-dns/variables.tf
+++ terraform-modules/external-dns/variables.tf
@@ -4,8 +4,6 @@ variable "target_project" {}
 variable "region" {}
 #credentials
 variable "target_credentials" {}
-#depends_on work around
-variable "depends_on" { default = [], type = "list" }
 #name of external dns_zone resource name
 variable "target_dns_resource_name" {}
 #name of external dns_zone name
